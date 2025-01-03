9281967a584d90608b3bbd015659ba79e2b3f176
Celso Jr <celsojrfull@gmail.com>
2025-01-03 23:33:02 +0400

    Add Visual Studio solution file and update project to .NET 6.0

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace cslogo
         {
             Util.CheckResult(SDL_Init(SDL_INIT_VIDEO));
 
-            var window = SDL_CreateWindow("C# bindings for SDL2",
+            nint window = SDL_CreateWindow("C# bindings for SDL2",
                 SDL_WINDOWPOS_UNDEFINED,
                 SDL_WINDOWPOS_UNDEFINED,
                 512,
@@ -20,15 +20,15 @@ namespace cslogo
 
             Util.CheckResult(window);
 
-            var renderer = SDL_CreateRenderer(window, -1, SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
-            var logoTexture = IMG_LoadTexture(renderer, "Assets/cslogo.jpg");
+            nint renderer = SDL_CreateRenderer(window, -1, SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+            nint logoTexture = IMG_LoadTexture(renderer, "Assets/cslogo.jpg");
 
             Util.CheckResult(SDL_RenderClear(renderer));
             Util.CheckResult(SDL_RenderCopy(renderer, logoTexture, IntPtr.Zero, IntPtr.Zero));
 
             SDL_RenderPresent(renderer);
-                
-            var quit = false;
+
+            bool quit = false;
             while (!quit && SDL_WaitEvent(out SDL_Event e) != 0)
             {
                 switch (e.type)
