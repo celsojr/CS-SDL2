0e45f40b5a012966300714a47217cd06bc21ce69
Celso Jr <celsojrfull@gmail.com>
2025-01-05 21:52:05 +0400

    Refactor Utils: Remove redundant CheckResult methods

--- src/Utils.cs
+++ src/Utils.cs
@@ -5,24 +5,6 @@ namespace SdlEngine
 {
     static class Utils
     {
-        internal static void CheckResult(object result)
-        {
-            if (result == default)
-            {
-                throw new InvalidOperationException(nameof(result),
-                    new Exception(SDL_GetError()));
-            }
-        }
-
-        internal static void CheckResult(int result)
-        {
-            if (result != default)
-            {
-                throw new InvalidOperationException(nameof(result),
-                    new Exception(SDL_GetError()));
-            }
-        }
-
         internal static void CheckSDLError(string context)
         {
             string error = SDL_GetError();
@@ -31,6 +13,5 @@ namespace SdlEngine
                 Console.WriteLine($"{context}: {error}");
             }
         }
-
     }
 }
