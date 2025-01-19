d5040c6c850c72b3ea8cdaba491f48a2d3ec2057
Celso Jr <celsojrfull@gmail.com>
2025-01-19 15:14:51 +0400

    Fixing the bomb count logic by avoiding the excessive use of SDL events

--- src/Minesweeper/MinesweeperCell.cs
+++ src/Minesweeper/MinesweeperCell.cs
@@ -6,12 +6,15 @@ using static SDL2.SDL;
 
 namespace Minesweeper
 {
+    // [StructLayout(LayoutKind.Sequential)]
     public class MinesweeperCell : Button
     {
         public bool HasBomb { get { return _hasBomb; } private set { _hasBomb = value; } }
         public int AdjacentBombs { get { return _adjacentBombs; } private set { _adjacentBombs = value; } }
-        public int Row { get { return _row; } }
-        public int Col { get { return _col; } }
+        public bool HasFlag => _hasFlag;
+        public int Row => _row;
+        public int Col => _col;
+        public bool IsCleared => _isCleared;
 
         private int _adjacentBombs;
         private readonly int _row;
@@ -33,12 +36,24 @@ namespace Minesweeper
         {
             AdjacentBombs = count;
 
-            if (count > 0)
-            {
-                _text.SetText(count.ToString(), Config.TEXT_COLORS[count]);
-            }
+            // if (_hasBomb)
+            // {
+            //     // cell._isCleared = true;
+            //     SetColor(Config.BUTTON_FAILURE_COLOR);
+            //     SetIsDisabled(true);
+            //     // _bombImage.Render();
+            // }
+            // else if (count > 0 && count > AdjacentBombs)
+            // {
+            //     _text.SetText(count.ToString(), Config.TEXT_COLORS[count]);
+            // }
         }
 
+        // public MinesweeperCell()
+        //     : base(0, 0, 0, 0)
+        // {
+            
+        // }
 
         public MinesweeperCell(int x, int y, int w, int h, int row, int col)
             : base(x, y, w, h)
@@ -47,15 +62,15 @@ namespace Minesweeper
             _col = col;
             _flagImage = new Image(x, y, w, h, "/Users/celsojr/Repos/CS-SDL2/Assets/flag.png");
             _bombImage = new Image(x, y, w, h, "/Users/celsojr/Repos/CS-SDL2/Assets/bomb.png");
-            // _text = new Text(x, y, w, h, "", Config.TEXT_COLORS[0]);
+            // _text = new Text(x, y, w, h, "", Config.TEXT_COLORS[0]); // "" non white space string is throwing
             _text = new Text(x, y, w, h, " ", Config.TEXT_COLORS[0]);
         }
 
-        public override void HandleEvent(in SDL_Event e, int adjBombs)
+        public override void HandleEvent(in SDL_Event e)
         {
             if (_isCleared) return;
 
-            base.HandleEvent(in e, adjBombs);
+            base.HandleEvent(in e);
 
             if (e.type == UserEvents.CELL_CLEARED)
             {
@@ -64,7 +79,7 @@ namespace Minesweeper
             }
             else if (e.type == UserEvents.BOMB_PLACED)
             {
-                HandleBombPlaced(in e.user, adjBombs);
+                HandleBombPlaced(in e.user);
                 // HandleBombPlaced(e);
             }
             else if (e.type == UserEvents.GAME_WON)
@@ -128,17 +143,8 @@ namespace Minesweeper
         public bool PlaceBomb()
         {
             if (_hasBomb) return false;
-
-            // Console.WriteLine("Cell bomb placed");
-
             _hasBomb = true;
-
             // ReportEvent(UserEvents.BOMB_PLACED);
-
-            // Handle adjacent bombs here
-
-
-
             return true;
         }
 
@@ -148,7 +154,16 @@ namespace Minesweeper
             _isCleared = true;
             SetIsDisabled(true);
             SetColor(Config.BUTTON_CLEARED_COLOR);
-            ReportEvent(UserEvents.CELL_CLEARED);
+
+
+            if (HasBomb)
+            {
+                SetColor(Config.BUTTON_FAILURE_COLOR);
+                ReportEvent(UserEvents.GAME_LOST);
+            }
+
+            // Reporting this type of event from here is not working
+            // ReportEvent(UserEvents.CELL_CLEARED);
         }
 
         private void ReportEvent(SDL_EventType eventType)
@@ -178,9 +193,20 @@ namespace Minesweeper
                 // Console.WriteLine($"Number of events in queue: {count}");
                 // 398 Filtered out a mouse motion event!
 
+                // int numTouchDevices = SDL_GetNumTouchDevices();
+                // for (int i = 0; i < numTouchDevices; i++)
+                // {
+                //     long touchId = SDL_GetTouchDevice(i);
+                //     Console.WriteLine($"Touch Device ID {i}: {touchId}");
+                //     // returning -1 on macOS arm64 which is not valid
+                // }
+
+
+                // SDL is faliling to push the event over here
+                // Custom pure c# event is needed
                 if (SDL_PushEvent(ref sdlEvent) != 0)
                 {
-                    Console.WriteLine($"Failed to push event: {SDL_GetError()}");
+                    // Console.WriteLine($"Failed to push event: {SDL_GetError()}");
                 }
             }
             finally
@@ -195,6 +221,11 @@ namespace Minesweeper
 
             GCHandle handle = GCHandle.FromIntPtr(e.data1);
 
+            if (handle.Target is MinesweeperCell otherTest && otherTest._hasBomb)
+            {
+                ReportEvent(UserEvents.GAME_LOST);
+            }
+
             if (handle.Target is MinesweeperCell other && IsAdjacent(other) && !other._hasBomb)
             {
                 ClearCell();
@@ -203,7 +234,7 @@ namespace Minesweeper
             handle.Free();
         }
 
-        private void HandleBombPlaced(in SDL_UserEvent e, int adjBombs)
+        private void HandleBombPlaced(in SDL_UserEvent e)
         {
             GCHandle handle = GCHandle.FromIntPtr(e.data1);
 
@@ -212,8 +243,8 @@ namespace Minesweeper
                 // _adjacentBombs++;
                 // ++_adjacentBombs;
                 ++_adjacentBombs;
-                Console.WriteLine(_adjacentBombs + " bombs placed");
-                Console.WriteLine(adjBombs + " bombs placed");
+                // Console.WriteLine(_adjacentBombs + " bombs placed");
+                // Console.WriteLine(adjBombs + " bombs placed");
                 // _text.SetText(_adjacentBombs.ToString(), Config.TEXT_COLORS[other._adjacentBombs]);
                 _text.SetText(_adjacentBombs.ToString(), Config.TEXT_COLORS[_adjacentBombs]);
                 // int adjacentBombs = other.CountAdjacentBombs();
@@ -223,7 +254,7 @@ namespace Minesweeper
             handle.Free();
         }
 
-        private bool IsAdjacent(MinesweeperCell other)
+        public bool IsAdjacent(MinesweeperCell other)
         {
             return !(other == this)
                 && Math.Abs(other._row - _row) <= 1
@@ -232,7 +263,7 @@ namespace Minesweeper
 
         protected override void HandleLeftClick()
         {
-            if (!_hasFlag && !_isCleared)
+            if (!_hasFlag)
             {
                 ClearCell();
             }
@@ -242,9 +273,22 @@ namespace Minesweeper
         {
             if (!_isCleared)
             {
-                _hasFlag = !_hasFlag;
-                ReportEvent(_hasFlag ? UserEvents.FLAG_PLACED : UserEvents.FLAG_CLEARED);
+                if (_hasFlag)
+                {
+                    _hasFlag = false;
+                    ReportEvent(UserEvents.FLAG_CLEARED);
+                }
+                else if (FlagCounter.GetFlagsCount > 0)
+                {
+                    _hasFlag = true;
+                    ReportEvent(UserEvents.FLAG_PLACED);
+                }
             }
+        }
+
+        internal void SetCleared()
+        {
+            ClearCell();
         }
     }
 }
--- src/Minesweeper/MinesweeperGrid.cs
+++ src/Minesweeper/MinesweeperGrid.cs
@@ -1,6 +1,6 @@
 using System;
 using Engine;
-using System.Runtime.InteropServices;
+// using System.Runtime.InteropServices;
 using System.Collections.Generic;
 
 using static SDL2.SDL;
@@ -42,9 +42,41 @@ namespace Minesweeper
 
         public void HandleEvent(in SDL_Event e)
         {
+            // This event never being triggered
             if (e.type == UserEvents.CELL_CLEARED)
             {
-                HandleCellCleared(in e.user);
+                // HandleCellCleared(in e.user);
+                HandleCellCleared(in e);
+
+                // _cellsToClear = 0;
+                // int flagCount = 0;
+
+                // foreach (MinesweeperCell child in _children)
+                // {
+                //     // int adjBombs = CountAdjacentBombs(child.Row, child.Col);
+                //     // child.HandleEvent(in e, adjBombs);
+
+                //     if (!child.IsCleared)
+                //     {
+                //         _cellsToClear++;
+                //     }
+
+                //     if (child.HasFlag)
+                //     {
+                //         flagCount++;
+                //     }
+
+                //     child.HandleEvent(in e);
+                // }
+
+                // Console.WriteLine("cells to clear: " + _cellsToClear);
+                // Console.WriteLine("total flags: " + flagCount);
+
+                // if (_cellsToClear - flagCount == 0)
+                // {
+                //     SDL_Event gameWonEvent = new SDL_Event { type = UserEvents.GAME_WON };
+                //     _ = SDL_PushEvent(ref gameWonEvent);
+                // }
             }
             else if (e.type == UserEvents.NEW_GAME)
             {
@@ -55,48 +87,118 @@ namespace Minesweeper
                 PlaceBombs();
             }
 
+            _cellsToClear = 0;
+            int flagCount = 0;
+            int bombCount = 0;
+
             foreach (MinesweeperCell child in _children)
             {
-                int adjBombs = CountAdjacentBombs(child.Row, child.Col);
-                child.HandleEvent(in e, adjBombs);
-            }
-        }
+                // int adjBombs = CountAdjacentBombs(child.Row, child.Col);
+                // child.HandleEvent(in e, adjBombs);
 
-        private void HandleCellCleared(in SDL_UserEvent e)
-        {
-            if (e.data1 == IntPtr.Zero)
-            {
-                throw new Exception("Invalid cell reference in user event.");
+                if (!child.IsCleared && !child.HasFlag)
+                {
+                    _cellsToClear++;
+                }
+
+                if (child.HasFlag && child.HasBomb)
+                {
+                    flagCount++;
+                }
+
+                if (child.HasBomb)
+                {
+                    bombCount++;
+                }
+
+                if (e.type == SDL_EventType.SDL_MOUSEBUTTONDOWN && !child.HasBomb)
+                {
+                    int adjacentBombs = CountAdjacentBombs(child.Row, child.Col);
+                    // int adjacentCleared = CountAdjacentCleared(child.Row, child.Col);
+
+                    if (adjacentBombs == 0)
+                    {
+                        child.SetCleared();
+                    }
+
+                    // if (adjacentBombs > 0)
+                    // {
+                    //     if (adjacentCleared > 0)
+                    //     {
+                    //         child.SetCleared();
+                    //     }
+                    // }
+                }
+
+                child.HandleEvent(in e);
             }
 
-            // MinesweeperCell cell = e.data1 as MinesweeperCell;
-            // MinesweeperCell cell = Marshal.PtrToStructure<MinesweeperCell>(e.data1);
-            // MinesweeperCellData cellData = Marshal.PtrToStructure<MinesweeperCellData>(e.data1);
-            GCHandle handle = GCHandle.FromIntPtr(e.data1);
+            if (e.type != SDL_EventType.SDL_USEREVENT
+                && e.type != SDL_EventType.SDL_MOUSEMOTION
+                && e.type != UserEvents.FLAG_PLACED
+                && e.type != UserEvents.GAME_WON)
+            {
+                // Console.WriteLine("Cells to clear: " + (_cellsToClear - 1));
+                // Console.WriteLine("Total flags: " + flagCount);
+                // Console.WriteLine("REMAINING: " + (_cellsToClear - bombCount - flagCount));
+                // Console.WriteLine("REMAINING: " + (_cellsToClear - flagCount));
+                // Console.WriteLine("Event: " + e.type);
 
-            if (handle.Target is MinesweeperCell cell && cell.HasBomb)
-            {
-                SDL_Event gameLostEvent = new SDL_Event { type = UserEvents.GAME_LOST };
-                _ = SDL_PushEvent(ref gameLostEvent);
-            }
-            else
-            {
-                _cellsToClear--;
-                if (_cellsToClear == 0)
+                // if (flagCount == (bombCount - 1))
+                if (flagCount == bombCount)
                 {
                     SDL_Event gameWonEvent = new SDL_Event { type = UserEvents.GAME_WON };
                     _ = SDL_PushEvent(ref gameWonEvent);
                 }
             }
 
-            handle.Free();
+        }
+
+        // private void HandleCellCleared(in SDL_UserEvent e)
+        private void HandleCellCleared(in SDL_Event e)
+        {
+            // GCHandle handle = default;
+            // if (e.type == UserEvents.CELL_CLEARED)
+            // {
+            //     if (e.user.data1 == IntPtr.Zero)
+            //     {
+            //         throw new Exception("Invalid cell reference in user event.");
+            //     }
+
+            //     // MinesweeperCell cell = e.user.data1 as MinesweeperCell;
+            //     // MinesweeperCell cell2 = Marshal.PtrToStructure<MinesweeperCell>(e.user.data1);
+            //     // MinesweeperCellData cellData = Marshal.PtrToStructure<MinesweeperCellData>(e.data1);
+            //      handle = GCHandle.FromIntPtr(e.user.data1);
+
+            //     if (handle.Target is MinesweeperCell cell && cell.HasBomb)
+            //     {
+            //         // SDL_Event gameLostEvent = new SDL_Event { type = UserEvents.GAME_LOST };
+            //         // _ = SDL_PushEvent(ref gameLostEvent);
+
+            //         if (cell.HasBomb)
+            //         {
+            //             // cell._isCleared = true;
+            //             cell.SetColor(Config.BUTTON_FAILURE_COLOR);
+            //         }
+            //         cell.SetIsDisabled(true);
+            //     }
+            // }
+            // else
+            // {
+                _cellsToClear--;
+                if (_cellsToClear == 0)
+                {
+                    SDL_Event gameWonEvent = new SDL_Event { type = UserEvents.GAME_WON };
+                    _ = SDL_PushEvent(ref gameWonEvent);
+                }
+            // }
+
+            // handle.Free();
         }
 
         private void PlaceBombs()
         {
             int bombsToPlace = Config.BOMB_COUNT;
-
-            // Double check this
             _cellsToClear = Config.GRID_COLUMNS * Config.GRID_ROWS - Config.BOMB_COUNT;
 
             while (bombsToPlace > 0)
@@ -110,7 +212,7 @@ namespace Minesweeper
                 }
             }
 
-            // UpdateAllAdjacentBombCounts();
+            UpdateAllAdjacentBombCounts();
         }
 
         // Aditional function
@@ -124,6 +226,38 @@ namespace Minesweeper
                     minesweeperCell.SetAdjacentBombs(adjacentBombs);
                 }
             }
+        }
+
+        private int CountAdjacentCleared(int row, int col)
+        {
+            int count = 0;
+
+            // Relative positions of all 8 possible neighbors
+            int[] dRow = [-1, -1, -1, 0, 0, 1, 1, 1];
+            int[] dCol = [-1, 0, 1, -1, 1, -1, 0, 1];
+
+            for (int i = 0; i < 8; i++)
+            {
+                int adjRow = row + dRow[i];
+                int adjCol = col + dCol[i];
+
+                // Ensure the adjacent cell is within bounds
+                if (adjRow >= 1 && adjRow <= Config.GRID_ROWS &&
+                    adjCol >= 1 && adjCol <= Config.GRID_COLUMNS)
+                {
+                    // TODO: Improve performance for large lists
+                    foreach (var child in _children)
+                    {
+                        if (child.Row == adjRow && child.Col == adjCol && child.IsCleared)
+                        {
+                            count++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return count;
         }
 
         // Aditional function
@@ -144,10 +278,19 @@ namespace Minesweeper
                 if (adjRow >= 1 && adjRow <= Config.GRID_ROWS &&
                     adjCol >= 1 && adjCol <= Config.GRID_COLUMNS)
                 {
-                    int index = (adjRow - 1) * Config.GRID_COLUMNS + (adjCol - 1);
-                    if (_children[index] is MinesweeperCell adjCell && adjCell.HasBomb)
+                    // TODO: Improve performance for large lists
+                    foreach (var child in _children)
                     {
-                        bombCount++;
+                        if (child.Row == adjRow && child.Col == adjCol && child.HasBomb)
+                        {
+                            bombCount++;
+                            break;
+                        }
+                        // else if (child.Row == adjRow && child.Col == adjCol && !child.HasBomb
+                        //     && child.IsAdjacent(new MinesweeperCell(0,0,0,0,adjRow,adjCol)))
+                        // {
+                        //     child.SetCleared();
+                        // }
                     }
                 }
             }
@@ -156,38 +299,37 @@ namespace Minesweeper
         }
 
         // Aditional function
-        private void UpdateAdjacentCells(int row, int col)
-        {
-            int[] dRow = [-1, -1, -1, 0, 0, 1, 1, 1];
-            int[] dCol = [-1, 0, 1, -1, 1, -1, 0, 1];
+        // private void UpdateAdjacentCells(int row, int col)
+        // {
+        //     int[] dRow = [-1, -1, -1, 0, 0, 1, 1, 1];
+        //     int[] dCol = [-1, 0, 1, -1, 1, -1, 0, 1];
 
-            for (int i = 0; i < 8; i++)
-            {
-                int adjRow = row + dRow[i];
-                int adjCol = col + dCol[i];
+        //     for (int i = 0; i < 8; i++)
+        //     {
+        //         int adjRow = row + dRow[i];
+        //         int adjCol = col + dCol[i];
 
-                if (adjRow >= 1 && adjRow <= Config.GRID_ROWS &&
-                    adjCol >= 1 && adjCol <= Config.GRID_COLUMNS)
-                {
-                    // int index = (adjRow - 1) * Config.GRID_COLUMNS + (adjCol - 1);
-                    int index = 1;
-                    if (_children[index] is MinesweeperCell adjCell && !adjCell.HasBomb)
-                    {
-                        adjCell.IncrementAdjacentBombCount();
-                    }
-                }
-            }
-        }
+        //         if (adjRow >= 1 && adjRow <= Config.GRID_ROWS &&
+        //             adjCol >= 1 && adjCol <= Config.GRID_COLUMNS)
+        //         {
+        //             int index = (adjRow - 1) * Config.GRID_COLUMNS + (adjCol - 1);
+        //             if (_children[index] is MinesweeperCell adjCell && !adjCell.HasBomb)
+        //             {
+        //                 adjCell.IncrementAdjacentBombCount();
+        //             }
+        //         }
+        //     }
+        // }
 
 
     }
 }
 
-[StructLayout(LayoutKind.Sequential)]
-public struct MinesweeperCellData
-{
-    public int Row;
-    public int Col;
-    public bool HasBomb;
-    public int AdjacentBombs;
-}
+// [StructLayout(LayoutKind.Sequential)]
+// public struct MinesweeperCellData
+// {
+//     public int Row;
+//     public int Col;
+//     public bool HasBomb;
+//     public int AdjacentBombs;
+// }
